5fd62420a4532fe9cbc2708afb42f67e4fc86a8c
dev <dev@chcMacBook-Air-3.local>
2015-07-19 02:07:17 +0800

    first

---
+++ Logger/Logger.h
@@ -0,0 +1,25 @@
+//
+//  Logger.h
+//  Logger
+//
+//  Created by dev on 3/7/15.
+//  Copyright (c) 2015 dev. All rights reserved.
+//
+
+#import <Cocoa/Cocoa.h>
+
+//! Project version number for Logger.
+FOUNDATION_EXPORT double LoggerVersionNumber;
+
+//! Project version string for Logger.
+FOUNDATION_EXPORT const unsigned char LoggerVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <Logger/PublicHeader.h>
+
+
+#import "DDLog.h"
+#import "DDLog+LOGV.h"
+#import "DDTTYLogger.h"
+#import "DDFileLoggerh"
+#import "DDASLLoggerh"
+#import "DDASLLogCapture.h"
